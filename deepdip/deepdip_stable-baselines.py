c5cd6e2f34ca66325592f2bdc4b58ee88f5ed09c
BlueDi <diogohmcruz@hotmail.com>
2019-06-03 18:23:17 +0100

    Remove unused Socket code
    Forgot to remove the old files after changing the gRPC.
    
    Remove OpenAIObserver
    
    Revert observer
    
    Revert remove observer

--- deepdip/deepdip_stable-baselines.py
+++ deepdip/deepdip_stable-baselines.py
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 gym_env_id = 'Diplomacy_Strategy-v0'
 algorithm = 'ppo2'
 total_timesteps = 1e6
-saving_interval = 8 #1 interval = 128 steps
+saving_interval = 1 #1 interval = 128 steps
 train_timesteps = 1e2
 best_mean_reward, n_steps = 0, 0
 
